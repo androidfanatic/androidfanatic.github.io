7b50916870d1de4695407f14acbb559f3bec62aa
androidfanatic <manish_raj@outlook.com>
2024-09-25 10:33:54 +0530

    update publish script

--- publish_index.sh
+++ publish_index.sh
@@ -1,1 +1,5 @@
 npx -y html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index.code.html >index.html
+git add index.code.html
+git add index.html
+git commit -m "Publish index.html"
+git push
